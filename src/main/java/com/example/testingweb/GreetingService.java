0f30d5d161f628b08bffa2ba8113b66d2963e23f
Arufonsu <17498701+Arufonsu@users.noreply.github.com>
2023-05-20 15:47:22 -0400

    Update azure-pipelines.yml for Azure Pipelines
    + Error para hacer trigger de umbral de SonarCloud con Blocker
    + Dockerfile

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+
+	private String password = "3214";
+
 	public String greet() {
 		return "Hello, World";
 	}

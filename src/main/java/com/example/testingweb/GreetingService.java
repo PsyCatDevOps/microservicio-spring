024c557d680898a041013a7cbb85aa0f4fb5049d
Arufonsu <17498701+Arufonsu@users.noreply.github.com>
2023-05-20 21:52:53 -0400

    fix: vulnerabilidad BLOCKER

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
-	private String password = "3214";
-
 	public String greet() {
 		return "Hello, World";
 	}
